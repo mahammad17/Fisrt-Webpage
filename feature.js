84c6d0b601b018566d0aba9e9406a2258b4cbf80
Mahammad Guluzada <mguluzada@gmail.com>
2022-06-26 18:41:26 +0400

    added new features to js file

--- feature.js
+++ feature.js
@@ -1,13 +1,68 @@
-let first = 35;
-let second = 30;
-let third = 25;
+// let first = 35;
+// let second = 30;
+// let third = 25;
 
-let total = first + second + (third*2);
+// let total = first + second + (third*2);
 
-let average = total/4;
+// let average = total/4;
 
-let finalprice = total - 5;
+// let finalprice = total - 5;
 
-console.log("Total Price: " + total);
-console.log("Average Price: " + average);
-console.log("Final Price: " + finalprice);
+// console.log("Total Price: " + total);
+// console.log("Average Price: " + average);
+// console.log("Final Price: " + finalprice);
+
+
+//TASK 1 -------------------------
+
+
+let n = 6;
+
+if (n%2==0){
+    console.log("Cut")
+}
+else{
+    console.log("Tek")
+}
+
+//TASK 2 ------------------------
+
+
+let b = 55;
+let a = 77;
+
+if(b>a){
+    console.log(b)
+}else if(b==a){
+    console.log("Equal")
+}else{
+    console.log(a)
+}
+
+//TASK 3 --------------------------
+
+let x = 4;
+let y = 8;
+let z = 11;
+
+if(x>(y,z)){
+    console.log(x)
+}else if(y>(x,z)){
+    console.log(y)
+}else {
+    console.log(z)
+}
+
+//TASK 4 ----------------------------
+
+let start = 15;
+let num = 12;
+let end = 10;
+
+if (num>start && num<end){
+    console.log(true)
+}else if (num>end && num<start){
+    console.log(true)
+}else{
+    console.log(false)
+}
