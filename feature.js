933e71559ac6fc343e270d39adbd078eb0572aa3
Mahammad Guluzada <mguluzada@gmail.com>
2022-06-26 14:12:51 +0400

    added js file

---
+++ feature.js
@@ -0,0 +1,13 @@
+let first = 35;
+let second = 30;
+let third = 25;
+
+let total = first + second + (third*2);
+
+let average = total/4;
+
+let finalprice = total - 5;
+
+console.log("Total Price: " + total);
+console.log("Average Price: " + average);
+console.log("Final Price: " + finalprice);
