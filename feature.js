7ce0da26b788d83bba93ecace84738bd785f420f
Mahammad Guluzada <mguluzada@gmail.com>
2022-06-27 16:10:59 +0400

    added new task

--- feature.js
+++ feature.js
@@ -15,6 +15,8 @@
 
 //TASK 1 -------------------------
 
+//1. variable-a eded daxil edirem, console-da mene ededin tek ve ya cut oldugunu qaytarmalidi. meselen 1 daxil etdimse consola tek cixarsin.
+
 
 let n = 6;
 
@@ -26,6 +28,8 @@ else{
 }
 
 //TASK 2 ------------------------
+
+//2. 2 variable teyin edirem. bu ededlerden boyuk olani consola cixarmalidi, eger beraberdise desin beraberdi
 
 
 let b = 55;
@@ -41,6 +45,9 @@ if(b>a){
 
 //TASK 3 --------------------------
 
+//3.  3 variable teyin edirem, en boyuk ededi consola cixarsin
+
+
 let x = 22;
 let y = 35;
 let z = 15;
@@ -55,9 +62,12 @@ if(x>y && x>z){
 
 //TASK 4 ----------------------------
 
-let start = 15;
+//4. 3 eded daxil edirem -> num, start ve end. consolda mene num-in start ve endin ortasinda yerlesdiyini ve ya yerlesmediyini cixarsin.
+
+
+let start = 19;
 let num = 16;
-let end = 17;
+let end = 11;
 
 if (num>start && num<end){
     console.log(true)
@@ -65,4 +75,24 @@ if (num>start && num<end){
     console.log(true)
 }else{
     console.log(false)
+}
+
+//TASK 5 ---------------------------
+
+// 5. bir year adinda variable teyin edirem (mes: 2012 qebul edir).
+// bu sertler altinda consola cixaracaqsiz.
+
+// If year is divisble by 4 and not divisble by 100 then print “leap year”.
+// Or if year is divisible by 400 then print “leap year”.
+// Else print “not a leap year”.
+
+
+let year = 2000;
+
+if(year%4==0 && year%100!=0){
+    console.log("Leap Year")
+}else if(year%400==0){
+    console.log("Leap Year")
+}else{
+    console.log("Nota a Leap Year")
 }
