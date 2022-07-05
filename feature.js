72f81aa9ba86fe7d405ba478c24b4a6cd956dafb
Mahammad Guluzada <mguluzada@gmail.com>
2022-07-05 21:02:38 +0400

    added newfeature.js file

--- feature.js
+++ feature.js
@@ -1,98 +1,98 @@
-// let first = 35;
-// let second = 30;
-// let third = 25;
+// // let first = 35;
+// // let second = 30;
+// // let third = 25;
 
-// let total = first + second + (third*2);
+// // let total = first + second + (third*2);
 
-// let average = total/4;
+// // let average = total/4;
 
-// let finalprice = total - 5;
+// // let finalprice = total - 5;
 
-// console.log("Total Price: " + total);
-// console.log("Average Price: " + average);
-// console.log("Final Price: " + finalprice);
-
-
-//TASK 1 -------------------------
-
-//1. variable-a eded daxil edirem, console-da mene ededin tek ve ya cut oldugunu qaytarmalidi. meselen 1 daxil etdimse consola tek cixarsin.
+// // console.log("Total Price: " + total);
+// // console.log("Average Price: " + average);
+// // console.log("Final Price: " + finalprice);
 
 
-let n = 6;
+// //TASK 1 -------------------------
 
-if (n%2==0){
-    console.log("EVEN")
-}
-else{
-    console.log("ODD")
-}
-
-//TASK 2 ------------------------
-
-//2. 2 variable teyin edirem. bu ededlerden boyuk olani consola cixarmalidi, eger beraberdise desin beraberdi
+// //1. variable-a eded daxil edirem, console-da mene ededin tek ve ya cut oldugunu qaytarmalidi. meselen 1 daxil etdimse consola tek cixarsin.
 
 
-let b = 55;
-let a = 77;
+// let n = 6;
 
-if(b>a){
-    console.log(b)
-}else if(b==a){
-    console.log("Equal")
-}else{
-    console.log(a)
-}
+// if (n%2==0){
+//     console.log("EVEN")
+// }
+// else{
+//     console.log("ODD")
+// }
 
-//TASK 3 --------------------------
+// //TASK 2 ------------------------
 
-//3.  3 variable teyin edirem, en boyuk ededi consola cixarsin
+// //2. 2 variable teyin edirem. bu ededlerden boyuk olani consola cixarmalidi, eger beraberdise desin beraberdi
 
 
-let x = 22;
-let y = 35;
-let z = 15;
+// let b = 55;
+// let a = 77;
 
-if(x>y && x>z){
-    console.log(x)
-}else if(y>x && y>z){
-    console.log(y)
-}else {
-    console.log(z)
-}
+// if(b>a){
+//     console.log(b)
+// }else if(b==a){
+//     console.log("Equal")
+// }else{
+//     console.log(a)
+// }
 
-//TASK 4 ----------------------------
+// //TASK 3 --------------------------
 
-//4. 3 eded daxil edirem -> num, start ve end. consolda mene num-in start ve endin ortasinda yerlesdiyini ve ya yerlesmediyini cixarsin.
-
-
-let start = 19;
-let num = 16;
-let end = 11;
-
-if (num>start && num<end){
-    console.log(true)
-}else if (num>end && num<start){
-    console.log(true)
-}else{
-    console.log(false)
-}
-
-//TASK 5 ---------------------------
-
-// 5. bir year adinda variable teyin edirem (mes: 2012 qebul edir).
-// bu sertler altinda consola cixaracaqsiz.
-
-// If year is divisble by 4 and not divisble by 100 then print “leap year”.
-// Or if year is divisible by 400 then print “leap year”.
-// Else print “not a leap year”.
+// //3.  3 variable teyin edirem, en boyuk ededi consola cixarsin
 
 
-let year = 2000;
+// let x = 22;
+// let y = 35;
+// let z = 15;
 
-if(year%4==0 && year%100!=0){
-    console.log("Leap Year")
-}else if(year%400==0){
-    console.log("Leap Year")
-}else{
-    console.log("Nota a Leap Year")
-}
+// if(x>y && x>z){
+//     console.log(x)
+// }else if(y>x && y>z){
+//     console.log(y)
+// }else {
+//     console.log(z)
+// }
+
+// //TASK 4 ----------------------------
+
+// //4. 3 eded daxil edirem -> num, start ve end. consolda mene num-in start ve endin ortasinda yerlesdiyini ve ya yerlesmediyini cixarsin.
+
+
+// let start = 19;
+// let num = 16;
+// let end = 11;
+
+// if (num>start && num<end){
+//     console.log(true)
+// }else if (num>end && num<start){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+// //TASK 5 ---------------------------
+
+// // 5. bir year adinda variable teyin edirem (mes: 2012 qebul edir).
+// // bu sertler altinda consola cixaracaqsiz.
+
+// // If year is divisble by 4 and not divisble by 100 then print “leap year”.
+// // Or if year is divisible by 400 then print “leap year”.
+// // Else print “not a leap year”.
+
+
+// let year = 2000;
+
+// if(year%4==0 && year%100!=0){
+//     console.log("Leap Year")
+// }else if(year%400==0){
+//     console.log("Leap Year")
+// }else{
+//     console.log("Nota a Leap Year")
+// }
---
+++ newtasks.js
@@ -0,0 +1,107 @@
+// TASK 1 --------------------------
+
+// 1. Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+
+// The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+
+// let num;
+
+// while (true) {
+//     num = prompt("Enter the number", "");
+//     if (num > 100) {
+//         break;
+//     }
+// }
+// console.log(num);
+
+
+//TASK 2 --------------------------------
+
+// 2. promptdan iki eded daxil edirem, birinci eded ikinci ise quvveti, meselen 2 sonra 3 daxil etdimse cavabda 8 cixarmalidi.
+
+
+// let number = prompt("Number","");
+// let square = prompt("Square","");
+
+// let squaredNumber = Math.pow(number,square);
+
+// console.log(squaredNumber);
+
+
+
+
+//TASK 3 --------------------------------
+
+// 3. promptdan eded daxil edirem ve consolda bu sekilde cixarmalidir. meselen 8 daxil etdimse
+
+// let i = prompt("Enter the number", "")
+
+// for (let j = 1; j <= i; j++) {
+
+//    console.log()
+//   }
+
+
+//TASK 4 --------------------------------
+
+// 4. reqem daxil edirem ve nece reqemli oldugunu tapir. meselen 1234 reqemi daxil etsem proqram 4 cixarmalidir. (Math.floor nedi arasdirarsiz.)
+
+// let entrnum = 345;
+
+
+//TASK 5 ---------------------------------
+
+//5. eded daxil edirem ve reqemlerinin cemini cixarir. meselen 1234 daxil etsem mene cavab olaraq 10 cixarmalidir.
+
+// let val = prompt("Enter the number", "")
+// let sum = 0;
+
+// while(val){
+//   sum += val % 10;
+//   val = Math.floor(val / 10);
+// }
+
+// console.log(sum);
+
+
+
+  //TASK 6 -------------------------------
+
+//   6. Write a JavaScript program which itegrates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+
+
+// for(i = 1; i <= 100; i++){
+//     if(i % 15 == 0){
+//     console.log("FizzBuzz");
+//     }
+//     else if(i % 5 == 0){
+//         console.log("Buzz");
+//      }
+//      else if(i % 3 == 0)
+//      {
+//          console.log("Fizz");
+//      }else{
+//          console.log(i);
+//      }
+// }
+
+//TASK 7 -----------------
+
+// 7. Write a JavaScript program to sum the multiples of 3 and 5 under 1000.
+
+let sumthree = 0, sumfive = 0;
+
+for(a = 1; a <= 1000; a++){
+  if(a % 3 == 0){
+  sumthree += a;
+}
+  else if(a % 5 == 0){
+  sumfive += a;
+  }
+}
+allsum = sumfive + sumthree;
+
+console.log("Three: " + sumthree,"Five: "+sumfive)
+console.log("ALLSUM: " + allsum);
+
+
